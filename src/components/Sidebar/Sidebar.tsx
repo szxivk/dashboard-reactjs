b267bf97a5a4f8f20b2b1949beda117ddb5e78e0
szxivk <50396748+szxivk@users.noreply.github.com>
2025-01-12 10:43:20 +0000

    Add search bar to sidebar component
    - also styled the search bar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { AccountToggle } from "./AccountToggle";
+import { Search } from "./Search";
 
 export const Sidebar = () => {
   return (
     <div>
       <div className="overflow-y-scroll sticky top-4 h-[calc(100vh-32px-48px)]">
         <AccountToggle />
+        <Search />
       </div>
       {/* plan toggle */}
     </div>
