91544b9d455ecd98c151b07bcca62966b2c7b4a0
szxivk <50396748+szxivk@users.noreply.github.com>
2025-01-12 08:28:08 +0000

    Update dashboard component a bit
    - formatted the code and removed element content

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 
 export const Dashboard = () => {
-  return (
-    <div className="bg-white rounded-lg pb-4 shadow  h-[200vh]">Dashboard</div>
-  );
+  return <div className="bg-white rounded-lg pb-4 shadow  h-[200vh]"></div>;
 };
