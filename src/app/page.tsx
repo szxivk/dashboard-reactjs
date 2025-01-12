ae99c40aa0475e9b0f3eeeb42119d8cbf314b681
szxivk <50396748+szxivk@users.noreply.github.com>
2025-01-12 08:26:53 +0000

    Update main page layout
    - set initial styles for sidebar and dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Sidebar } from "@/components/Sidebar/Sidebar";
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="grid gap-4 p-4 grid-cols-[220px,_1fr]">
       <Sidebar />
       <Dashboard />
     </main>
