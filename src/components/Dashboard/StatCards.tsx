0225c48bb338dcb0355785665da9d828493c154f
szxivk <50396748+szxivk@users.noreply.github.com>
2025-01-14 05:18:38 +0000

    Update info and their style on stat-cards
    - created a separate top section with info relayed to it through props

--- src/components/Dashboard/StatCards.tsx
+++ src/components/Dashboard/StatCards.tsx
@@ -1,25 +1,26 @@
 import React from "react";
+import { FiTrendingDown, FiTrendingUp } from "react-icons/fi";
 
 export const StatCards = () => {
   return (
     <>
       <Card
         title="Gross Revenue"
-        value="₹1,04,00,799.11"
+        value="₹1.04cr."
         pillText="2.75%"
         trend="up"
         period="From Jul 1st - Dec 31st"
       />
       <Card
         title="Avg Order"
-        value="₹2,339.12"
+        value="₹2.3K"
         pillText="1.01%"
         trend="down"
         period="From Jul 1st - Dec 31st"
       />
       <Card
         title="Trailing year"
-        value="₹2,40,78,843"
+        value="₹2.40cr."
         pillText="60.75%"
         trend="up"
         period="Previous 365 days"
@@ -41,5 +42,26 @@ const Card = ({
   trend: "up" | "down";
   period: string;
 }) => {
-  return <div className="p-4 rounded col-span-4 border border-stone-300"></div>;
+  return (
+    <div className="p-4 rounded col-span-4 border border-stone-300">
+      <div className="flex mb-8 items-start justify-between">
+        <div>
+          <h3 className="text-stone-500 mb-2 text-sm">{title}</h3>
+          <p className="text-3xl font-semibold">{value}</p>
+        </div>
+
+        <span
+          className={`text-xs flex items-center gap-1 font-medium px-2 py-1 rounded ${
+            trend === "up"
+              ? "bg-green-100 text-green-700"
+              : "bg-red-100 text-red-700"
+          }`}
+        >
+          {trend === "up" ? <FiTrendingUp /> : <FiTrendingDown />}
+          {pillText}
+        </span>
+      </div>
+      <p className="text-xs text-stone-500">{period}</p>
+    </div>
+  );
 };
