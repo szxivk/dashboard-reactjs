dc4b4fbb41325641962fb545a410826bf661ddcf
szxivk <50396748+szxivk@users.noreply.github.com>
2025-01-14 09:57:23 +0000

    Add usage radar chart to the dashboard grid
    - styled but needs to be fine tuned

--- src/components/Dashboard/Grid.tsx
+++ src/components/Dashboard/Grid.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { StatCards } from "./StatCards";
 import { ActivityGraph } from "./ActivityGraph";
+import { UsageRadar } from "./UsageRadar";
 
 export const Grid = () => {
   return (
     <div className="px-4 grid gap-3 grid-cols-12">
       <StatCards />
       <ActivityGraph />
+      <UsageRadar />
     </div>
   );
 };
